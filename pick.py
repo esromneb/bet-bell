a367587fced45e6ae611a0ab1c544fc0ce6266f6
x <x>
2016-12-01 09:39:35 +0000

    gen other pickle

--- pick.py
+++ pick.py
@@ -5,8 +5,16 @@ o['app_id'] = ''
 o['key']=''
 o['secret']=''
 
-print repr(o)
+#print repr(o)
 
 pickle.dump( o, open( "server.p", "wb" ) )
 
+
+o = {}
+o['key'] = '' # same as above
+
+pickle.dump( o, open( "client.p", "wb" ) )
+
+
 o = pickle.load( open( "server.p", "rb" ) )
+o = pickle.load( open( "client.p", "rb" ) )
